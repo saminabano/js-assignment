f30379fb00c337709cfaa65ca7748615713c1051
saminabano <saminaasif33@gmail.com>
2023-04-09 11:46:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//QUESTION # 1//
+alert("welcome");
+//QUESTION # 2//
+alert("Error!please enter a valid password");
+//QUESTION # 3//
+alert("welcome To Js Land..\nHappy Coding");
+//QUESTION# 4//
+alert("welcome To Js Land");
+alert("Happy Coding");
+//QUESTION # 5//
+alert("Hello I Can Run Js Through My Web Browse Console");
+
+

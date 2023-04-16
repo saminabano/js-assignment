601a4a3ac5ccb1fe6d97e13db841f727456efe4a
saminabano <saminaasif33@gmail.com>
2023-04-16 11:46:30 +0500

    change

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 //QUESTION # 1//
-alert("welcome");
+//lert("welcome");//
 //QUESTION # 2//
-alert("Error!please enter a valid password");
+//alert("Error!please enter a valid password");//
 //QUESTION # 3//
-alert("welcome To Js Land..\nHappy Coding");
+//alert("welcome To Js Land..\nHappy Coding");//
 //QUESTION# 4//
-alert("welcome To Js Land");
-alert("Happy Coding");
+//alert("welcome To Js Land");//
+//alert("Happy Coding");//
 //QUESTION # 5//
-alert("Hello I Can Run Js Through My Web Browse Console");
+//alert("Hello I Can Run Js Through My Web Browse Console");//
 
+//Assignment#2
+//Question #1
+var username=("");
+
+//---------------------------------------
+//Question#2
+var myName=("saminaBano");
+//----------------------------------------
+//Question#3
+var message=("Hello World");
+alert (message);
+//----------------------------------------
+//Question#4
+var studentdata=("jhone doe");
+alert(studentdata);
+var studentdata=("15 years old");
+alert(studentdata);
+var studentdata=("certified Mobile application Development");
+alert (studentdata);
+//----------------------------------------
+//Question#5
+var enimation=("pizza\npizz\npiz\npi\np");
+alert (enimation); 
+//---------------------------------------
+//Question#6
+var email=("My Email address is"+" saminaasif33@gmail.com");
+alert(email);
+//------------------------------------------
+
+//Question # 7
+var book =("A smarter way to learn JAVASCRIPT");
+alert ("I am trying to learn from the book"+ book);
+//-----------------------------------------
+
+//Question #7
+var message=("Yah! I can write HTML content through JAVASCRIPT");
+document.write(message);
+//---------------------------------------
+//Question # 8
+var show=("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+alert (show);
+document.write(show);
 
